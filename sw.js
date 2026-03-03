e4951b9662485c813dec0579f2c40869bd9ad189
vesatukio <vesatukio@gmail.com>
2026-03-03 12:40:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,44 @@
-self.addEventListener('install', e=>{
-e.waitUntil(
-caches.open('dutaled').then(cache=>{
-return cache.addAll(['./']);
-})
-);
+const CACHE_NAME = "dutaled-v2";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e=>{
-e.respondWith(
-caches.match(e.request).then(res=>res||fetch(e.request))
-);
+// ACTIVATE (hapus cache lama)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
