86526925a6b5add9148e1ca0fe358569a6504601
vesatukio <vesatukio@gmail.com>
2026-05-01 12:00:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,9 +8,39 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting(); // 🔥 TAMBAH INI
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
-
 self.addEventListener("fetch", event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME){
+            return caches.delete(key); // 🔥 hapus cache lama
+          }
+        })
+      )
+    )
+  );
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim(); // 🔥 TAMBAH INI
 });
